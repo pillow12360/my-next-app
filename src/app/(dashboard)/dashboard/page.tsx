555bec53abe22b20dcc22765e04b46464ec7d373
pillow12360 <ehdcks1256@naver.com>
2025-03-05 16:23:04 +0900

    feat: 레이아웃 테스트

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <>
+      <div className="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl" />
+      <div className="bg-muted/50 mx-auto h-full w-full max-w-3xl rounded-xl" />
+    </>
+  )
+}
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,7 +13,11 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 
-export default function Page() {
+export default function DashboardLayout({
+                                          children,
+                                        }: {
+  children: React.ReactNode
+}) {
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -40,8 +44,7 @@ export default function Page() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 px-4 py-10">
-          <div className="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl" />
-          <div className="bg-muted/50 mx-auto h-full w-full max-w-3xl rounded-xl" />
+          {children}
         </div>
       </SidebarInset>
     </SidebarProvider>
